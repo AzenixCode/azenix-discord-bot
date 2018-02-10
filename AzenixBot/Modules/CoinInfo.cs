8323d3c2d03f4b90f73c24e760adb89bdd903cdb
dnovacik <novacik.daniel@gmail.com>
2018-02-10 10:34:28 +0100

    Initial commit

---
+++ AzenixBot/Modules/CoinInfo.cs
@@ -0,0 +1,22 @@
+﻿using Discord;
+using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzenixBot.Modules
+{
+    public class CoinInfo : ModuleBase<SocketCommandContext>
+    {
+        [Command("info")]
+        public async Task CoinInfoAsync()
+        {
+            EmbedBuilder builder = new EmbedBuilder();
+
+            builder.WithTitle("Azenix [XAZ]").WithDescription("Zero Fee Currency");
+
+            await this.ReplyAsync(string.Empty, false, builder.Build());
+        }
+    }
+}
